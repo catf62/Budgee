24f9eed78cc2f382b5249f45af53932e9d7c70ad
Catriona Ford <catriona.b.ford@gmail.com>
2018-11-12 19:48:12 +0000

    initialize method added and tested for budget_transaction class

--- budget_app_code/models/specs/budget_transaction_spec.rb
+++ budget_app_code/models/specs/budget_transaction_spec.rb
@@ -4,3 +4,20 @@ require_relative('../budget_transaction')
 require( 'pry-byebug' )
 
 class BudgetTest < Minitest::Test
+
+  def setup
+    options = {
+      'id' => 1,
+      'budget_id' => 1,
+      'transaction_id' => 1
+    }
+
+    @budget_transaction1 = BudgetTransaction.new(options)
+  end
+
+  def test_budget_transaction_has_budget_id
+    p @budget_transaction1
+    assert_equal(1, @budget_transaction1.budget_id)
+  end
+
+end
