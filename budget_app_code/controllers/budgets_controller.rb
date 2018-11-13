e567a1d57203c32eaf476bde152cae665e94d986
Catriona Ford <catriona.b.ford@gmail.com>
2018-11-13 15:53:56 +0000

    new and create routes added for budgets

--- budget_app_code/controllers/budgets_controller.rb
+++ budget_app_code/controllers/budgets_controller.rb
@@ -4,5 +4,16 @@ require_relative( '../models/budget.rb' )
 also_reload( '../models/*' )
 
 get '/budgets' do
-  "Hello World"
+  @budgets = Budget.all()
+  erb ( :"budgets/index" )
+end
+
+get '/budgets/new' do
+  erb ( :"budgets/new" )
+end
+
+post '/budgets' do # create
+  budget = Budget.new( params )
+  budget.save()
+  redirect to( "budgets" )
 end
