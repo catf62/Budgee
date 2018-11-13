48cfa07e1b845e3884894285729381fb9b389a79
Catriona Ford <catriona.b.ford@gmail.com>
2018-11-13 10:54:12 +0000

    /merchant and /merchant/new pages added - still testing

---
+++ budget_app_code/controllers/merchants_controller.rb
@@ -0,0 +1,18 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/merchant.rb' )
+also_reload( '../models/*' )
+
+get '/merchants' do
+  @merchants = Merchant.all()
+  erb ( :"merchants/index" )
+end
+
+get '/merchants/:id' do
+  @merchant = Merchant.find(params['id'].to_i)
+  erb( :"merchants/show" )
+end
+get '/merchants/new' do
+
+  erb ( :"merchants/new" )
+end
